d262b49db51754b7a953a30d2f5a16479b09cbe1
ytouate <touateyoussef2003@gmail.com>
2022-11-08 17:48:58 +0100

    implementing a simple iterator temmplate class

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: ytouate <touateyoussef2003@gmail.com>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/11/03 12:47:40 by ytouate           #+#    #+#              #
-#    Updated: 2022/11/06 17:01:56 by ytouate          ###   ########.fr        #
+#    Updated: 2022/11/06 20:43:55 by ytouate          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,14 +14,14 @@ FILE = ./file.cpp
 
 NAME = main.exe
 
-CPPFLAGS = -std=c++11 #-Wall -Wextra -Werror
+CPPFLAGS = -std=c++98 #-Wall -Wextra -Werror
 
 O_FILE = $(FILE:.cpp=.o)
 
 all: $(NAME)
 
 $(NAME) : $(O_FILE)
-	@c++ -Wall -Wextra -Werror -std=c++11 -fsanitize=address -g $(O_FILE) -o $(NAME)
+	@c++ -Wall -Wextra -Werror -std=c++98 -fsanitize=address -g $(O_FILE) -o $(NAME)
 
 clean:
 	@rm -rf *.o
--- iterator.hpp
+++ iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: ytouate <touateyoussef2003@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 16:08:00 by ytouate           #+#    #+#             */
-/*   Updated: 2022/11/06 19:17:49 by ytouate          ###   ########.fr       */
+/*   Updated: 2022/11/08 17:46:09 by ytouate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,61 +16,62 @@
 #include <iostream>
 #include <iterator>
 #include <set>
+
+template <class T>
 class iterator
 {
-
 public:
     typedef std::random_access_iterator_tag iterator_category;
     typedef ptrdiff_t difference_type;
-    typedef int value_type;
-    typedef int *pointer;
-    typedef int &reference;
-    iterator(pointer ptr) : m_ptr(ptr) {}
-    reference operator*(void) { return *m_ptr; }
-    pointer operator->(void) { return m_ptr; }
-    iterator operator++(void)
+    typedef  T value_type;
+    typedef  T *pointer;
+    typedef  T &reference;
+    iterator(pointer ptr) : _ptr(ptr) {}
+    reference operator*(void) { return *_ptr; }
+    pointer operator->(void) { return _ptr; }
+    iterator operator+=(difference_type n)
     {
-        m_ptr++;
-        return *this;
+        this->_ptr += n;
+        return this->_ptr;
     }
-    iterator operator++(int)
+    iterator operator-=(difference_type n)
     {
-        iterator temp = *this;
-        m_ptr++;
-        return temp;
+        this->_ptr -= n;
     }
     iterator operator--(void)
     {
-        --m_ptr;
-        return *this;
-    }
-    iterator operator--(int)
-    {
         iterator temp = *this;
-        m_ptr--;
+        this->_ptr--;
         return temp;
     }
-    friend bool operator==(iterator const &right, iterator const &left) { return right.m_ptr == left.m_ptr; }
-    friend bool operator!=(iterator const &right, iterator const &left) { return right.m_ptr != left.m_ptr; }
-    friend bool operator<(iterator const &right, iterator const &left) { return right.m_ptr < left.m_ptr; }
-    friend bool operator>(iterator const &right, iterator const &left) { return right.m_ptr > left.m_ptr; }
-    friend bool operator<=(iterator const &right, iterator const &left) { return right.m_ptr <= left.m_ptr; }
-    friend bool operator>=(iterator const &right, iterator const &left) { return right.m_ptr >= left.m_ptr; }
-
-    
+    iterator operator++(void)
+    {
+        iterator temp = *this;
+        this->_ptr++;
+        return temp;
+    }
+    iterator operator++(int notUsed)
+    {
+        (void)notUsed;
+        this->_ptr++;
+        return *this;
+    }
+    iterator operator--(int notUsed)
+    {
+        (void)notUsed;
+        this->_ptr--;
+        return *this;
+    }
+    friend bool operator==(iterator const &right, iterator const &left) { return right._ptr == left._ptr; }
+    friend bool operator!=(iterator const &right, iterator const &left) { return right._ptr != left._ptr; }
+    friend bool operator<(iterator const &right, iterator const &left) { return right._ptr < left._ptr; }
+    friend bool operator>(iterator const &right, iterator const &left) { return right._ptr > left._ptr; }
+    friend bool operator<=(iterator const &right, iterator const &left) { return right._ptr <= left._ptr; }
+    friend bool operator>=(iterator const &right, iterator const &left) { return right._ptr >= left._ptr; }
 
     ~iterator() {}
-
 private:
-    pointer m_ptr;
+    T *_ptr;
 };
 
-// iterator::iterator()
-// {
-// }
-
-// iterator::~iterator()
-// {
-// };
-
-#endif // MACRO
+#endif
