e25f7e3c403e1a2f859a1bea7917e2d9f1fd8426
ytouate <touateyoussef2003@gmail.com>
2022-11-14 14:40:59 +0100

    working with allocators on my vector

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: ytouate <ytouate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 12:47:26 by ytouate           #+#    #+#             */
-/*   Updated: 2022/11/14 13:18:28 by ytouate          ###   ########.fr       */
+/*   Updated: 2022/11/14 14:39:20 by ytouate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@
 #include "iterator.hpp"
 #include "reverse_iterator.hpp"
 #include "vector/vector.hpp"
+#include <memory>
 
 class Integers
 {
@@ -50,53 +51,61 @@ public:
 int main(void)
 {
 
-    Integers ints;
+    ft::vector<int> myVec(10, 10);
+    ft::vector<int>::iterator it = myVec.begin();
+    ft::vector<int>::iterator ite = myVec.end();
+    
+    for (; it != ite; ++it)
+        std::cout << *it << std::endl;
+    return 0;
 
-    ft::iterator<int> it = ints.begin();
-    ft::iterator<int> ite = ints.end();
-
-    ft::reverse_iterator<ft::iterator<int> > rit = ints.rbegin();
-    ft::reverse_iterator<ft::iterator<int> > rite = ints.rend();
-
-    // rit++;
-    // rit--;
-
-    std::vector<int> vec;
-    for (int i = 0; i < 200; i++)
-    {
-        vec.push_back(i + 1);
-    }
-
-    std::vector<int>::reverse_iterator rit2 = vec.rbegin();
-    std::vector<int>::reverse_iterator rite2 = vec.rend();
-    ft::reverse_iterator<ft::iterator<int> > another(rit);
-    rite = rit;
-    // rite--;
-    // std::cout << *rite2 << std::endl;
-    std::cout << *(rite - 1) << std::endl;
     // Integers ints;
-    // iterator<Integers> it = ints.begin();
-    // iterator<Integers> ite = ints.end();
-    // (void)ints, (void)it, (void)ite;
-    // (*it).name = "Something else";
-    // it->name = "Hello world";
+
+    // ft::iterator<int> it = ints.begin();
+    // ft::iterator<int> ite = ints.end();
+ 
+    // ft::reverse_iterator<ft::iterator<int> > rit = ints.rbegin();
+    // ft::reverse_iterator<ft::iterator<int> > rite = ints.rend();
+
+    // // rit++;
+    // // rit--;
 
     // std::vector<int> vec;
-    // vec.push_back(10);
-    // vec.push_back(20);
-
-    // // std::random_access_iterator_tag something;
-    // std::vector<int>::iterator::iterator_type
-    // // std::vector<int>::iterator it2 = vec.begin();
-    // // std::vector<int>::iterator ite2 = vec.end();
-    // std::vector<int>::iterator rit = vec.begin();
-    // std::vector<int>::iterator rite = vec.end();
-
-    // ++it;
-    // while (rit < rite)
+    // for (int i = 0; i < 200; i++)
     // {
-    //     std::cout << *rit << std::endl;
-    //     rit++;
+    //     vec.push_back(i + 1);
     // }
-    return (0);
+
+    // std::vector<int>::reverse_iterator rit2 = vec.rbegin();
+    // std::vector<int>::reverse_iterator rite2 = vec.rend();
+    // ft::reverse_iterator<ft::iterator<int> > another(rit);
+    // rite = rit;
+    // // rite--;
+    // // std::cout << *rite2 << std::endl;
+    // std::cout << *(rite - 1) << std::endl;
+    // // Integers ints;
+    // // iterator<Integers> it = ints.begin();
+    // // iterator<Integers> ite = ints.end();
+    // // (void)ints, (void)it, (void)ite;
+    // // (*it).name = "Something else";
+    // // it->name = "Hello world";
+
+    // // std::vector<int> vec;
+    // // vec.push_back(10);
+    // // vec.push_back(20);
+
+    // // // std::random_access_iterator_tag something;
+    // // std::vector<int>::iterator::iterator_type
+    // // // std::vector<int>::iterator it2 = vec.begin();
+    // // // std::vector<int>::iterator ite2 = vec.end();
+    // // std::vector<int>::iterator rit = vec.begin();
+    // // std::vector<int>::iterator rite = vec.end();
+
+    // // ++it;
+    // // while (rit < rite)
+    // // {
+    // //     std::cout << *rit << std::endl;
+    // //     rit++;
+    // // }
+    // return (0);
 }
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: ytouate <ytouate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 12:54:33 by ytouate           #+#    #+#             */
-/*   Updated: 2022/11/14 13:12:58 by ytouate          ###   ########.fr       */
+/*   Updated: 2022/11/14 14:37:57 by ytouate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,29 +17,55 @@
 #include "../iterator.hpp"
 #include <iostream>
 
-template <class T, class Allocator = std::allocator<T> >
-class vector
-{
-private:
-    size_t len;
-public:
-    typedef typename Allocator::reference reference;
-    typedef typename Allocator::const_reference const_reference;
-    typedef int size_type;
-    typedef ptrdiff_t difference_type;
-    typedef T value_type;
-    typedef Allocator allocator_type;
-    typedef typename Allocator::pointer pointer;
-    typedef ft::iterator<T> iterator;
-    typedef typename Allocator::const_pointer const_pointer;
-    typedef ft::reverse_iterator<iterator> reverse_iterator;
-    vector()
+namespace ft
+{    
+    template <class T, class Allocator = std::allocator<T> >
+    class vector
     {
-        len = 0;
-    }
-    vector &operator=(vector const &rhs);
-    vector(vector const &obj);
-    ~vector();
-};
+    private:
+        size_t len;
+        Allocator _alloc;
+        T *vec;
+    public:
+        typedef typename Allocator::reference reference;
+        typedef typename Allocator::const_reference const_reference;
+        typedef size_t size_type;
+        typedef ptrdiff_t difference_type;
+        typedef T value_type;
+        typedef Allocator allocator_type;
+        typedef typename Allocator::pointer pointer;
+        typedef ft::iterator<T> iterator;
+        typedef typename Allocator::const_pointer const_pointer;
+        typedef ft::reverse_iterator<iterator> reverse_iterator;
+        
+
+        iterator begin()
+        {
+            return ft::iterator<T> (&vec[0]);
+        }
+        iterator end()
+        {
+            return ft::iterator<T> (&vec[len]);
+        }
+        vector(const Allocator &alloc = Allocator())
+        {
+            len = 0;
+            // alloc.allocate(len);
+        }
+        vector(size_type n, const T& value = T(), const Allocator &alloc = Allocator())
+        {
+            len = n;
+            this->_alloc = alloc;
+            this->vec = this->_alloc.allocate(n);
+            for (int i = 0; i < n; i++)
+                this->vec[i] = n;
+        }
+        vector &operator=(vector const &rhs);
+        vector(vector const &obj);
+        ~vector()
+        {
+        }
+    };
+}
 
 #endif // VECTOR_HPP
