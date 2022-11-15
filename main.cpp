8b5eba995928826edfd64509650d1d5a7e1b8fb5
ytouate <touateyoussef2003@gmail.com>
2022-11-15 11:28:54 +0100

    implementing more of the vector member functions and the const iterators

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: ytouate <ytouate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 12:47:26 by ytouate           #+#    #+#             */
-/*   Updated: 2022/11/14 23:17:45 by ytouate          ###   ########.fr       */
+/*   Updated: 2022/11/15 11:12:59 by ytouate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,33 +25,37 @@
 #include "vector/vector.hpp"
 #include <memory>
 
-class Integers
-{
-private:
-    int vec[200];
-    int size;
+// class Integers
+// {
+// private:
+//     int vec[200];
+//     int size;
 
-public:
-    ft::iterator<int> begin() { return ft::iterator<int>(&vec[0]); }
-    ft::iterator<int> end() { return ft::iterator<int>(&vec[200]); }
-    ft::reverse_iterator<ft::iterator<int> > rbegin() { return ft::reverse_iterator<ft::iterator<int> >(&vec[199]); }
-    ft::reverse_iterator<ft::iterator<int> > rend() { return ft::reverse_iterator<ft::iterator<int> >(&vec[size - (size + 1)]); }
-    void f() { std::cout << "hello world\n"; }
-    Integers() : size(200)
-    {
-        std::srand(time(NULL));
-        for (int i = 0; i < 200; i++)
-        {
-            vec[i] = i + 1;
-        }
-    }
-    ~Integers() {}
-};
+// public:
+//     ft::iterator<int> begin() { return ft::iterator<int>(&vec[0]); }
+//     ft::iterator<int> end() { return ft::iterator<int>(&vec[200]); }
+//     ft::reverse_iterator<ft::iterator<int> > rbegin() { return ft::reverse_iterator<ft::iterator<int> >(&vec[199]); }
+//     ft::reverse_iterator<ft::iterator<int> > rend() { return ft::reverse_iterator<ft::iterator<int> >(&vec[size - (size + 1)]); }
+//     void f() { std::cout << "hello world\n"; }
+//     Integers() : size(200)
+//     {
+//         std::srand(time(NULL));
+//         for (int i = 0; i < 200; i++)
+//         {
+//             vec[i] = i + 1;
+//         }
+//     }
+//     ~Integers() {}
+// };
 
 int main(void)
 {
 
-    ft::vector<int> myVec(10, 10);
+    std::vector<int> i;
+    ft::vector<int> myVec;
+    
+    // std::cout << i.back() << std::endl;
+    return 0;
     ft::vector<int>::iterator it = myVec.begin();
     ft::vector<int>::iterator ite = myVec.end();
     std::vector<int> v;
@@ -64,6 +68,7 @@ int main(void)
     }
     ft::vector<int>::reverse_iterator rit = myVec.rbegin();
     ft::vector<int>::reverse_iterator rite = myVec.rend();
+
 
     // for (; rit != rite; ++rit)
     //     std::cout << *rit << std::endl;
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: ytouate <ytouate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 12:54:33 by ytouate           #+#    #+#             */
-/*   Updated: 2022/11/14 23:17:09 by ytouate          ###   ########.fr       */
+/*   Updated: 2022/11/15 11:28:21 by ytouate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,11 @@ namespace ft
     private:
         size_t len;
         Allocator _alloc;
-        size_t capacity;
+        size_t _capacity;
         T *vec;
 
     public:
+        // types
         typedef typename Allocator::reference reference;
         typedef typename Allocator::const_reference const_reference;
         typedef size_t size_type;
@@ -37,9 +38,17 @@ namespace ft
         typedef Allocator allocator_type;
         typedef typename Allocator::pointer pointer;
         typedef ft::iterator<T> iterator;
+        typedef const iterator const_iterator;
         typedef typename Allocator::const_pointer const_pointer;
         typedef ft::reverse_iterator<iterator> reverse_iterator;
+        typedef const reverse_iterator const_reverse_iterator;
+
+        // element access
         reference operator [] (size_type n)  { return this->vec[n]; }
+        reference front() { return this->vec[0]; }
+        const_reference front() const { return this->vec[0]; }
+        reference back() { return this->vec[this->len - 1]; }
+        const_reference back() const { return this->vec[this->len - 1]; }
         const_reference operator [] (size_type n) const { return this->vec[n]; }
         const_reference at(size_type n) const
         {
@@ -53,21 +62,32 @@ namespace ft
                 throw std::out_of_range("Index out of range");
             return this->vec[n];
         }
-        reference front();
-        const_reference front() const;
-        reference back();
-        const_reference back() const;
-        size_type max_size() const { return this->_alloc.max_size(); }
+        
+        // iterators
+        const_iterator begin() const { return ft::iterator <T> (&this->vec[0]); }
+        const_iterator end() const { return ft::iterator<T>  (&this->vec[this->len]); }
         iterator begin() { return ft::iterator<T>(&vec[0]); }
-        iterator end() { return ft::iterator<T>(&vec[len]); }
-        reverse_iterator rbegin() { return ft::reverse_iterator <ft::iterator <T> > (&vec[len - 1]); }
+        iterator end() { return ft::iterator<T>(&vec[this->len]); }
+        const_reverse_iterator rbegin() const { return ft::reverse_iterator < ft::iterator <T> > (&this->vec[this->len - 1]); }
+        reverse_iterator rbegin() { return ft::reverse_iterator <ft::iterator <T> > (&vec[this->len - 1]); }
         reverse_iterator rend() { return ft::reverse_iterator <ft::iterator <T> > (&vec[-1]); }
-        vector(const Allocator &alloc = Allocator())
+        const_reverse_iterator rend() const { return ft::reverse_iterator <ft::iterator<T> > (&this->vec[-1]); }
+        
+        // capacity
+        size_type max_size() const { return this->_alloc.max_size(); }
+        size_type size() const { return this->len; }
+        size_type capacity() const { return this->_capacity; }
+        bool empty() const { return this->len == 0; }
+        void reserve(size_type n);
+        void resize(size_type sz, T c = T());
+
+        // construction / destruction / assignment / copy construction
+        explicit vector(const Allocator &alloc = Allocator())
         {
             this->len = 0;
             this->vec = this->_alloc.allocate(this->len);
         }
-        vector(size_type n, const T &value = T(), const Allocator &alloc = Allocator())
+        explicit vector(size_type n, const T &value = T(), const Allocator &alloc = Allocator())
         {
             len = n;
             this->_alloc = alloc;
@@ -75,16 +95,10 @@ namespace ft
             for (int i = 0; i < n; i++)
                 this->vec[i] = n;
         }
-        
         allocator_type get_allocator() const { return this->_alloc; }
-        size_type size() const { return this->len; }
-        
-        // unimplimented functions
-        vector &operator=(vector const &rhs);
+        vector &operator=(const vector<T, Allocator> & rhs);
         vector(vector const &obj);
-        ~vector()
-        {
-        }
+        ~vector() {}
     };
 }
 
